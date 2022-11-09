bfd12fc54105aae7dbbf8e3239f96204db482be6
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-11-08 19:54:05 -0500

    Add task2 test

--- task1.js
+++ task1.js
@@ -2,7 +2,7 @@ function stringLength(string) {
   if (string.length < 10 && string.length > 0) {
     return string.length;
   } else {
-    return new Error('The string short');
+    throw new Error('The string is too short or long');
   }
 }
 
--- task1.test.js
+++ task1.test.js
@@ -1,5 +1,11 @@
 const task1 = require('./task1.js');
 
-it('stringLength', () => {
+test('stringLength', () => {
   expect(task1('hello')).toBe(5);
+});
+
+test('stringLengthError', () => {
+  expect(() => {
+    task1('');
+  }).toThrow('The string is too short or long');
 });
